b6267419d45b7d94c1b2730ce4155ac134936301
Talha Abdul Sattar <148429044+Talha8899@users.noreply.github.com>
2026-09-20 15:33:04 +0500

    Delete Backend directory

--- Backend/App/routers/root.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router=APIRouter(tags=["root"])
-
-@router.get("/")
-def root():
-    """Health check-api is running"""
-    return {"message":"welcome-Root is running"}
